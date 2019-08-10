82e85df74a1cfc2d0e3458aab79409506d62576c
atctwo <dr.whojones@gmail.com>
2019-08-10 23:31:53 +0100

    Removed build folder

--- build/libraries/Adafruit-GFX-Library/glcdfont.c.d
+++
@@ -1,2 +0,0 @@
-D:\programming\arduino\watch2\build\libraries\Adafruit-GFX-Library\glcdfont.c.o: \
- C:\Users\alexm\personalpathprograms\arduino-PR-beta1.9-BUILD-107\libraries\Adafruit-GFX-Library\glcdfont.c
